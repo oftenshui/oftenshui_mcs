105d67ac38bcfc49082fbf6f3805d4516ba8555c
oftenshui <88616541+oftenshui@users.noreply.github.com>
2025-03-25 11:54:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -273,7 +273,8 @@ async def mcs(self, message: AstrMessageEvent):
 
     return CommandResult().message(result_text).use_t2i(False)
 
-    
+
+
     @filter.command("一言")
     async def hitokoto(self, message: AstrMessageEvent):
         """来一条一言"""
